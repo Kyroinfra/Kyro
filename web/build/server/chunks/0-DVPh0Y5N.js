209cf415cb79c619934778ecaa25554a3c282eee
Nyxox-debut <numterminal@gmail.com>
2026-04-10 14:58:43 +0100

    stop tracking build

--- web/build/server/chunks/0-DVPh0Y5N.js
+++
@@ -1,9 +0,0 @@
-const index = 0;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_layout.svelte-B0mCk13D.js')).default;
-const imports = ["_app/immutable/nodes/0.BMgiyQjA.js","_app/immutable/chunks/YCwS6Nwd.js","_app/immutable/chunks/zU3tt0ZV.js","_app/immutable/chunks/M79jyEj8.js","_app/immutable/chunks/DsGEnUUF.js","_app/immutable/chunks/BLd6RmJV.js","_app/immutable/chunks/DcJwU2AC.js","_app/immutable/chunks/jEOsyNKi.js"];
-const stylesheets = ["_app/immutable/assets/0.8t8re2F-.css"];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=0-DVPh0Y5N.js.map
--- web/build/server/chunks/_server.ts-DAO6dj4b.js
+++
@@ -1,8 +0,0 @@
-import { j as json } from './index-aGhFOaj4.js';
-
-function GET() {
-  return json({ status: "ok", timestamp: (/* @__PURE__ */ new Date()).toISOString() });
-}
-
-export { GET };
-//# sourceMappingURL=_server.ts-DAO6dj4b.js.map
